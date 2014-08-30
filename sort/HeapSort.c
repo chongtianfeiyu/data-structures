5adf07158742930d80c424227721bb84ed6b0a9a
ZhengYingHao <zyh1010110@163.com>
2014-08-30 15:17:06 +0800

    base HeapSort

--- sort/HeapSort.c
+++ sort/HeapSort.c
@@ -32,8 +32,41 @@ ElementType DeleteMin(PriorityQueue P)
     if (P == NULL)
     {
         printf("PriorityQueue is NULL.");
-        return 0;
+        exit(1);
     }
+    if (P->Size <= 0)
+    {
+        printf("No");
+        exit(2);
+    }
+    int i, child;
+    ElementType result = P->Elements[1];
+    ElementType lastElement = P->Elements[P->Size--]; //长度已减
+    
+    for (i = 1; i * 2 <= P->Size; i = child)
+    {
+        child = i * 2;
+        if (child <= P->Size && P->Elements[child] > P->Elements[child + 1])
+            child += 1;
+
+        if (lastElement > P->Elements[child])
+            P->Elements[i] = P->Elements[child];
+        else
+            break;
+    }
+    P->Elements[i] = lastElement;
+
+    //PrintArray("after delete", P->Elements, 1, P->Size);
+    return result;
+}
+
+void PrintArray(const char* message, ElementType A[], int begin, int end)
+{
+    int i;
+    printf("%s : ", message);
+    for (i = begin; i < end; ++i)
+        printf("%0.1lf ", A[i]);
+    printf("\n");
 }
 
 int main(int argc, const char *argv[])
@@ -43,9 +76,21 @@ int main(int argc, const char *argv[])
     P = (PriorityQueue)malloc(sizeof(BinaryHeap));
 
     scanf("%d", &N);
-    P->Elements = (ElementType *) malloc(sizeof(ElementType) * N);
-    P->Capacity = 10000;
+    P->Elements = (ElementType *) malloc(sizeof(ElementType) * (N + 1));
+    P->Capacity = N + 1;
     P->Size = 0;
+    
+    int i;
+    ElementType t;
+    for (i = 1; i <= N; ++i)
+    {
+        scanf("%lf", &t);
+        Insert(t, P);
+    }
+    PrintArray("all", P->Elements, 1, N + 1);
 
+    while (N--)
+        printf("%0.1lf ", DeleteMin(P));
+    printf("\n");
     return 0;
 }
